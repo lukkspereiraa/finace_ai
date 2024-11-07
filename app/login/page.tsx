6d4bc0cdfe4a42ab613f8e944c5cbc963dd9074e
lukkspereiraa <monkeydlukks@gmail.com>
2024-11-06 22:34:57 -0300

    feat: add authentication whit clerk

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,17 @@
 import Image from "next/image";
 import { Button } from "../_components/ui/button";
 import { LogInIcon } from "lucide-react";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const LoginPage = () => {
+
+const LoginPage = async () => {
+    const { userId } = await auth()
+    if (userId) {
+        redirect("/")
+    }
+
     return (<div className="grid grid-cols-2 h-full">
         {/*Esquerda*/}
         <div className="flex h-full flex-col justify-center p-8 max-w-[550px] mx-auto">
@@ -13,10 +22,13 @@ const LoginPage = () => {
 
             <p className="text-muted-foreground mb-8">A Finance AI é uma plataforma de gestão financeira que utiliza IA para monitorar suas movimentações, e oferecer insights personalizados, facilitando o controle do seu orçamento.</p>
 
-            <Button variant={"outline"}>
-                <LogInIcon className="mr-2" />
-                Fazer login ou criar conta
-            </Button>
+            <SignInButton>
+                <Button variant={"outline"}>
+                    <LogInIcon className="mr-2" />
+                    Fazer login ou criar conta
+                </Button>
+            </SignInButton>
+
         </div>
         {/*DIreita*/}
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
-import Image from "next/image";
-import { Button } from "./_components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { dark } from '@clerk/themes'
 
-export default function Home() {
+const Home = async () => {
+  const { userId } = await auth()
+  if (!userId) {
+    redirect("/login")
+  }
   return (
-    <Button>Hallo word</Button>
-  );
+    <div className="h-full flex items-center justify-center">
+      <UserButton showName />
+    </div>
+  );;
 }
+
+export default Home;
